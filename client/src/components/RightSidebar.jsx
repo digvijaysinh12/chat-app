39cfb159cee28685c1fc67367d68e297d4e0bffc
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2025-05-18 13:55:42 +0530

    Give UI to RightSidebar Component

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from 'react';
+import assets from '../assets/assets';
 
-const RightSidebar = () => {
-  return (
-    <div className='border border-amber-50'>
-      <h1 className='text-cyan-100 border-amber-50'>RightSidebar</h1>
+const RightSidebar = ({ selectedUser }) => {
+  return selectedUser && (
+    <div>
+      <div className="flex flex-col items-center text-white p-4 gap-2">
+        <img
+          src={selectedUser?.profilePic || assets.avatar_icon}
+          alt=""
+          className="w-20 aspect-[1/1] rounded-full"
+        />
+        <h1 className="text-xl font-medium flex items-center gap-2">
+          <span className="w-2 h-2 rounded-full bg-green-500"></span>
+          {selectedUser.fullName}
+        </h1>
+        <p className="text-center text-sm text-gray-300">{selectedUser.bio}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default RightSidebar
+export default RightSidebar;
