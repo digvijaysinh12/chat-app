222664130b32157efc6d5020ed0b0f638a9374b8
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2025-05-20 22:30:58 +0530

    Working on profile Page Ui

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import useNavigate from 'react';
 
 const ProfilePage = () => {
+
+  const [selectedImg,setSelectedImg] = useState(null)
+  const navigate = useNavigate(null);
+
   return (
-    <div>
-      <h1>Profile Page</h1>
+    <div className='min-h-screen bg-cover bg-no-repeat flex items-center justify-center'>
+      <div className='w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg'>
+        <form className='flex flex-col gap-5 p-10 flex-1'>
+          <h3 className='text-lg'>Profile details</h3>
+          <label htmlFor='avatar' className='flex items-center gap-3 cursor-pointer'>
+            <input type='file' id='avatar' accept='.png, .jpg, .jpeg' hidden/>
+            <img src={} alt='' />
+          </label>
+        </form>
+        <img src='' alt=''/>
+      </div>
     </div>
   )
 }
