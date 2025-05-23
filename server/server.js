531ab71017346573a3cc9cbb97a2a2ef834bbf99
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2025-05-23 11:07:34 +0530

    server test first api create

--- server/server.js
+++ server/server.js
@@ -8,3 +8,15 @@ import http from "http";
 const app = express();
 //We are using this http create server beacause the socket io support this http server
 const server = http.createServer(app)
+
+
+// Middleware setup
+app.use(express.json({limit: "4mb"}));
+// allow all the URL to connect our backend
+app.use(cors());
+
+app.use("/api/status", (req,res)=> res.send("Server is live"))
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, ()=> console.log("Server is running on PORT: "+PORT));
