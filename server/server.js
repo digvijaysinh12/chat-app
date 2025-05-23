3e027fd8c1242582d28a829a7cf414e481c5cda8
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2025-05-23 11:52:51 +0530

    Implement Successfull connection with database

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import http from "http";
+import { connectDB } from "./lib/db.js";
 
 
 //Create Express app and HTTP server
@@ -17,6 +18,9 @@ app.use(cors());
 
 app.use("/api/status", (req,res)=> res.send("Server is live"))
 
+
+// Connect to MongoDN
+await connectDB();
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, ()=> console.log("Server is running on PORT: "+PORT));
