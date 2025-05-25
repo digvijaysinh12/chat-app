0f1cb536084d2faa96b7b2a78f73592be01e55b7
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2025-05-25 10:50:55 +0530

    add update profile controller in userController

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/User.js";
 
@@ -59,4 +60,30 @@ export const login = async(req,res) => {
 //Controller to check if user is authenticated
 export const checkAuth= (req,res) => {
     res.json({success:true,user:req.user});
+}
+
+
+//Controller to update user profile details 
+
+export const updateProfile = async(req,res) =>{
+    try{
+        const {profilePic, bio, fullName} = req.body;
+
+        const userId = req.user._id;
+
+        let updatedUser;
+
+        if(!profilePic){
+            updatedUser = await User.findByIdAndUpdate(userId,{bio, fullName}, {new:true})
+        }else{
+            const upload = await cloudinary.uploader.upload(profilePic);
+            
+            updatedUser = await User.findByIdAndUpdate(userId, {profilePic: upload.secure_url, bio, fullName}, {new:true})
+        }
+        res.json({success:true, user: updatedUser})
+        
+    }catch(error){
+        console.log(error);
+        res.json({success:false, message: error.message});
+    }
 }
