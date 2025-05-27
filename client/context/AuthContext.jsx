4c5af0ac8e52a294e8f44147bb86898f8b3b3898
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2025-05-27 11:37:35 +0530

    add login call request

--- client/context/AuthContext.jsx
+++ client/context/AuthContext.jsx
@@ -30,8 +30,22 @@ export const AuthProvider = ({ children }) => {
 
     // Login function to handle user authentication and socket connection
     
-    const login = async() => {
-        
+    const login = async(state,credentials) => {
+        try{
+            const {data} = await axios.post(`/api/auth/${state}`,credentials);
+            if(data.success){
+                setAuthUser(data.userData);
+                connectSocket(data.userData);
+                axios.defaults.headers.common["token"] = data.tokenl;
+                setToken(data.token);
+                localStorage.setItem("token", data.token)
+                toast.success(data.message);
+            }else{
+                toast.error(data.message);
+            }
+        }catch(error){
+            toast.error(error.message);
+        }
     }
     //Connect socket funtion to handle socket connection and online users updates
     const connectSocket = (userData) =>{
