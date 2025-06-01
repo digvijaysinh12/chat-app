39cdc3768327194a92ea97505682a36b1c54f17d
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2025-06-01 12:04:55 +0530

    Remove unless props

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -17,7 +17,7 @@ const HomePage = () => {
         }`}
       >
         <Sidebar/>
-        <ChatContainer selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+        <ChatContainer/>
         {<RightSidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>}
       </div>
     </div>
