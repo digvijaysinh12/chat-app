b29ae9cc3db9ba37fe0c01e0b1dbc300a9da9e2b
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2025-06-01 12:03:30 +0530

    get all users search and new messages count functionlity with fixed error

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -6,7 +6,7 @@ import { ChatContext } from '../../context/chatContext';
 
 const Sidebar = () => {
 
-    const {getUsers, users, selectedUser, setSelectedUser, unSeenMessages, setUnseenMessages} = useContext(ChatContext);
+    const {getUsers, users, selectedUser, setSelectedUser, unseenMessages, setUnseenMessages} = useContext(ChatContext);
 
     const{logout, onlineUsers} = useContext(AuthContext);
 
@@ -76,7 +76,11 @@ const Sidebar = () => {
                                 <span className="text-neutral-400 text-xs">Offline</span>
                             }
                         </div>
-                        {unSeenMessages?.[user._id] > 0 && <p className='absolute top-4 right-4 text-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-violet-500/50'>{unSeenMessages[user._id]}</p>}
+{unseenMessages?.[user._id] > 0 && 
+  <p className='absolute top-4 right-4 text-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-violet-500/50'>
+    {unseenMessages[user._id]}
+  </p>
+}
                     </div>
                 ))}
             </div>
