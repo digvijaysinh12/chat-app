68a0e580e69d980b9d0e8afb43e9c4b9a3803e1b
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2025-06-01 12:04:33 +0530

    implement messages and integrate this file  with chatContext file

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,23 +1,56 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import assets, { messagesDummyData } from '../assets/assets';
+import { ChatContext } from '../../context/chatContext';
+import { AuthContext } from '../../context/AuthContext';
+import toast from 'react-hot-toast';
 
-const ChatContainer = ({ selectedUser, setSelectedUser }) => {
-  const loggedInUserId = '680f5116f10f3cd28382ed02'; // Assuming this is the logged-in user (you can make it dynamic later)
+const ChatContainer = () => {
 
-  // Filter messages relevant to selected user
-  const relevantMessages = messagesDummyData.filter(
-    (msg) =>
-      (msg.senderId === loggedInUserId && msg.receiverId === selectedUser?._id) ||
-      (msg.receiverId === loggedInUserId && msg.senderId === selectedUser?._id)
-  );
+  const { messages,selectedUser,setSelectedUser,sendMessage,getMessages} = useContext(ChatContext);
+  const {authUser,onlineUsers } = useContext(AuthContext);
+
   
   const scrollEnd = useRef()
 
+  const[input, setInput] = useState('');
+
+  // Handle sending a message
+  const handleSendMessage = async(e) => {
+    e.preventDefault();
+    if(input.trim() =="") return null;
+    await sendMessage({text:input.trim()});
+    setInput("");
+  }   
+
+  // Handle sending an Image
+  const handleSendImage = async(e) =>{
+    const file = e.target.files[0];
+    if(!file || !file.type.startsWith("image/")){
+      toast.error("select an image file");
+      return;
+    }
+
+    const reader = new FileReader();
+
+    reader.onload = async() => {
+      await sendMessage({image : reader.result})
+      e.target.value =""
+    }
+
+    reader.readAsDataURL(file);
+
+  }
+
+  useEffect(()=> {
+    if(selectedUser){
+      getMessages(selectedUser._id);
+    }
+  },[selectedUser])
   useEffect(() =>{
-    if(scrollEnd.current){
+    if(scrollEnd.current && messages){
       scrollEnd.current.scrollIntoView();
     }
-  },[])
+  },[messages])
   return selectedUser ? (
     <div className='h-full overflow-scroll relative backdrop:backdrop-blur-lg'>
 
@@ -26,7 +59,7 @@ const ChatContainer = ({ selectedUser, setSelectedUser }) => {
         <img src={selectedUser?.profilePic || assets.avatar_icon} alt='' className='w-8 rounded-full' />
         <p className='flex-1 text-lg text-white flex items-center gap-2'>
           {selectedUser.fullName}
-          <span className='w-2 h-2 rounded-full bg-green-500'></span>
+          {onlineUsers.includes(selectedUser._id)}<span className='w-2 h-2 rounded-full bg-green-500'></span>
         </p>
         <img
           onClick={() => setSelectedUser(null)}
@@ -39,11 +72,11 @@ const ChatContainer = ({ selectedUser, setSelectedUser }) => {
 
       {/* ---- chat area ---- */}
       <div className='flex flex-col h-[calc(100%-120px)] overflow-y-scroll p-3 pb-6'>
-        {relevantMessages.length > 0 ? (
-          relevantMessages.map((msg, index) => (
+        {messages.length > 0 ? (
+          messages.map((msg, index) => (
             <div
               key={index}
-              className={`flex items-end gap-2 ${msg.senderId === loggedInUserId ? 'justify-end' : 'justify-start'}`}
+              className={`flex items-end gap-2 ${msg.senderId === authUser._id ? 'justify-end' : 'justify-start'}`}
             >
               {msg.image ? (
                 <img
@@ -54,7 +87,7 @@ const ChatContainer = ({ selectedUser, setSelectedUser }) => {
               ) : (
                 <p
                   className={`p-2 max-w-[200px] md:text-sm font-light rounded-lg mb-8 break-all bg-violet-500/30 text-white ${
-                    msg.senderId === loggedInUserId ? 'rounded-br-none' : 'rounded-bl-none'
+                    msg.senderId === authUser._id ? 'rounded-br-none' : 'rounded-bl-none'
                   }`}
                 >
                   {msg.text}
@@ -63,8 +96,8 @@ const ChatContainer = ({ selectedUser, setSelectedUser }) => {
               <div className='text-center text-xs'>
                 <img
                   src={
-                    msg.senderId === loggedInUserId
-                      ? assets.avatar_icon
+                    msg.senderId === authUser._id
+                      ? authUser?.profilePic || assets.avatar_icon
                       : selectedUser?.profilePic || assets.avatar_icon
                   }
                   alt=''
@@ -83,9 +116,11 @@ const ChatContainer = ({ selectedUser, setSelectedUser }) => {
     {/* ---- bottom area ---- */}
     <div className='absolute bottom-0 left-0 right-0 flex items-center gap-3 p-3'>
         <div className='flex-1 flex items-center bg-gray-100/12 px-3 rounded-full'>
-          <input type='text' placeholder='Send a message' 
+          <input onChange={(e) => setInput(e.target.value)} value={input} 
+          onKeyDown={(e) => e.key === "Enter" ? handleSendMessage(e) : null} 
+          type='text' placeholder='Send a message' 
           className='flex-1 text-sm p-3 border-none rounded-lg outline-none text-white placeholder-gray-400'/>
-          <input type='file' id='image' accept='image/png, image/jpeg' hidden/>
+          <input onChange={handleSendImage} type='file' id='image' accept='image/png, image/jpeg' hidden/>
           <label htmlFor='image'>
             <img src={assets.gallery_icon} alt='' className='w-5 mr-2 cursor-pointer'/>
           </label>
@@ -96,7 +131,7 @@ const ChatContainer = ({ selectedUser, setSelectedUser }) => {
     </div>
   ) : (
     <div className='flex flex-col items-center justify-center gap-2 text-gray-500 bg-white/10 max-md:hidden'>
-      <img src={assets.logo_icon} className='max-w-16' alt='' />
+      <img onClick={handleSendMessage} src={assets.logo_icon} className='max-w-16' alt='' />
       <p className='text-lg font-medium text-white'>Chat Anytime, anywhere</p>
     </div>
   );
