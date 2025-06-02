6f0feb42cd0955bfb93921cdb08e2911503cfba0
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2025-06-02 10:12:31 +0530

    update HomePage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import Sidebar from '../components/Sidebar';
 import ChatContainer from '../components/ChatContainer';
 import RightSidebar from '../components/RightSidebar';
 import { AuthContext } from '../../context/AuthContext';
+import { ChatContext } from '../../context/chatContext';
 
 const HomePage = () => {
-  const [selectedUser, setSelectedUser] = useState(null);
 
+  const {selectedUser,setSelectedUser} = useContext(ChatContext);
+  
   return (
     <div className="w-full h-screen sm:px-[15%] sm:py-[5%] border border-amber-50">
       <div
