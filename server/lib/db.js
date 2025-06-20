6cf5dff4b4765d80942b16231899d05540831937
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2025-06-21 00:21:43 +0530

    some debug changes

--- server/lib/db.js
+++ server/lib/db.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
 // Function to connect to the mognodb database
-export const connectDB = async() => {
+export const connectDB = async(req,res) => {
     try{
         mongoose.connection.on('connected',()=> console.log('Database Connected'));
         await mongoose.connect(`${process.env.MONGODB_URI}/chatApp`)
     }catch(error){
-        console.log(error);
+            res.status({success:false,message:error.message})
     }
 }
