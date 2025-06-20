41a0e1356dfc46ce610f5a676483d8cce45bf9ef
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2025-06-20 14:15:07 +0530

    Create Rest api for send otp and Verify Otp

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,14 @@
 import express from 'express';
-import { checkAuth, login, signup, updateProfile } from '../controllers/userController.js';
+import { checkAuth, login, sendOtp, signup, updateProfile, verifyOtp } from '../controllers/userController.js';
 import { protectRoute } from '../middleware/auth.js';
 
 const userRouter = express.Router();
 
+
 userRouter.post("/signup",signup);
 userRouter.post("/login",login);
+userRouter.post("/send-otp",sendOtp);
+userRouter.post("/verify-otp",verifyOtp);
 userRouter.put("/update-profile",protectRoute,updateProfile);
 userRouter.get("/check",protectRoute,checkAuth);
 
