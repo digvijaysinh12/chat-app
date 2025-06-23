9772f370402114112bf94705bedbe7282d2ebaef
digvijaysinh12 <sarvaiyaraa040@gmail.com>
2025-06-23 19:31:01 +0530

    add some comments to understand code very well

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,15 +1,25 @@
 import express from 'express';
-import { checkAuth, login, sendOtp, signup, updateProfile, verifyOtp } from '../controllers/userController.js';
+import {
+  checkAuth,
+  login,
+  sendOtp,
+  signup,
+  updateProfile,
+  verifyOtp
+} from '../controllers/userController.js';
+
 import { protectRoute } from '../middleware/auth.js';
 
 const userRouter = express.Router();
 
+//Public Routes
+userRouter.post("/signup", signup);             // Register 
+userRouter.post("/login", login);               // Login 
+userRouter.post("/send-otp", sendOtp);          // Send OTP 
+userRouter.post("/verify-otp", verifyOtp);      // Verify OTP
 
-userRouter.post("/signup",signup);
-userRouter.post("/login",login);
-userRouter.post("/send-otp",sendOtp);
-userRouter.post("/verify-otp",verifyOtp);
-userRouter.put("/update-profile",protectRoute,updateProfile);
-userRouter.get("/check",protectRoute,checkAuth);
+//Protected Routes(Auth Token)
+userRouter.put("/update-profile", protectRoute, updateProfile);  // Update user profile
+userRouter.get("/check", protectRoute, checkAuth);               // Check if token is valid
 
 export default userRouter;
